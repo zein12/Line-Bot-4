13f138f7a30b6d033c631f005b41f431ec0a4739
gAiN <siraphob@gmail.com>
2016-10-25 01:16:52 +0700

    test

--- bot.php
+++ bot.php
@@ -276,7 +276,7 @@ Hope you enjoy :)'
 				$headers = array('Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 				//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 				$result = curl_exec($ch);
