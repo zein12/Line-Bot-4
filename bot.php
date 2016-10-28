08d659ebae7f78df6b24ed0653732e72c91b87cb
gAiN <siraphob@gmail.com>
2016-10-28 16:59:21 +0700

    test

--- bot.php
+++ bot.php
@@ -355,7 +355,8 @@ Hope you enjoy :)'
 
 			echo $result . "\r\n";
 
-			$log = file_get_contents('http://www.osk130.com/gain/linebot.php?uid='.$uid.'&text='.$event['message']['text']);
+			$url = 'http://www.osk130.com/gain/linebot.php?uid='.$uid.'&text='.$event['message']['text'];
+			$log = file_get_contents($url);
 		}
 	}
 }
