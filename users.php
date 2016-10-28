c2f6763c1dd690d33e29c5b23c7dc8ae621dbd40
gAiN <siraphob@gmail.com>
2016-10-28 17:02:29 +0700

    t

--- users.php
+++ users.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = 'UjY+N6xBQrgchpjZB7IU0ck2aypFc37LrIEozsryFd9WZTdI0fNj8xFyi5RCMzJ9e1N/c3JQt8w0XBBw1I6x4ev2Gwkyl8SHPibsqTS1hzdK6MoPza9/lwcQgttNrZWXGJYNsGJi2/ZgQ5TFflEG9wdB04t89/1O/w1cDnyilFU=';
-			$url = 'http://www.osk130.com/gain/linebot.php?uid='.$uid.'&text='.$event['message']['text'];
+			$uid=5;
+			$url = 'http://www.osk130.com/gain/linebot.php?uid='.$uid.'&text=test'];
 			$log = file_get_contents($url);
 			echo $log;
 			// //Get user profile but can't search userId
