cf5cb86079664217ab4051ab3d112344d7a9e8a3
gAiN <siraphob@gmail.com>
2016-10-28 16:50:38 +0700

    edit users

--- users.php
+++ users.php
@@ -1,8 +1,9 @@
 <?php
 $access_token = 'UjY+N6xBQrgchpjZB7IU0ck2aypFc37LrIEozsryFd9WZTdI0fNj8xFyi5RCMzJ9e1N/c3JQt8w0XBBw1I6x4ev2Gwkyl8SHPibsqTS1hzdK6MoPza9/lwcQgttNrZWXGJYNsGJi2/ZgQ5TFflEG9wdB04t89/1O/w1cDnyilFU=';
 
-			Get user profile but can't search userId
-			$userId = 'Udc964c94321d2db87bc8f17041ae37ea';
+			//Get user profile but can't search userId
+			//$userId = 'Udc964c94321d2db87bc8f17041ae37ea';
+			$userId = $_GET['uid'];
 			$url = 'https://api.line.me/v2/bot/profile/';
 			$headers = array('Authorization: Bearer ' . $access_token);
 			$ch = curl_init();
